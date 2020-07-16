8997fcfa64366f9d4fac4efb06f30fb562c76f3d
Rida R Syed <ridarafisyed@yahoo.com>
2020-07-16 18:05:33 +0500

    cleaning

--- src/components/productsList.js
+++ src/components/productsList.js
@@ -1,18 +1,41 @@
-import React, { useState, useContext } from "react"
+import React from "react"
 import { Link } from "gatsby"
 import Grid from "@material-ui/core/Grid"
-import CartContext from "../hooks/CartContext"
+
 import products from "../data/shoes_data2.json"
 
 import styled from "../styling/main.module.css"
 
-export default function products() {
-  const [itemNumber, setItemNumber] = useState(0)
-
-  const addToCart = () => {
-    console.log("added item into the cart")
-  }
+export default function products2() {
   return (
-    
+    <div className={styled.products}>
+      <Grid container spacing={3}>
+        {products.map((product, index) => {
+          return (
+            <Grid item lg={4} xs={12} sm={6} key={index}>
+              <div className={styled.card}>
+                <div className={styled.image_container}>
+                  <img
+                    src={product.image_thumb}
+                    className={styled.cardimage}
+                    alt={product.title}
+                  />
+                </div>
+
+                <div className={`styled.cardBody text-center`}>
+                  <h3 className={styled.cardTitle}>{product.name}</h3>
+                  <span>Rs: {product.price} </span>
+                </div>
+                <div className={styled.cardLink}>
+                  <Link to={`/products/${product.slug}`}>View Details</Link>
+                  {"  "}
+                  <Link to={`/products/${product.slug}`}>Add to Cart</Link>
+                </div>
+              </div>
+            </Grid>
+          )
+        })}
+      </Grid>
+    </div>
   )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,47 +1,20 @@
 import React from "react"
 import Layout from "../components/layout"
-
+import { graphql } from "gatsby"
 import { Link } from "gatsby"
 import Grid from "@material-ui/core/Grid"
+import ProductsList from "../components/productsList"
 
 import styled from "../styling/main.module.css"
 
-function products() {
+const products = () => {
   return (
     <Layout>
-      <div>
+      <div className={styled.container}>
         <div>
           <h1 className="text-center">Hello form Products Page </h1>
         </div>
-        <div className={styled.products}>
-          <Grid container spacing={3}>
-            {products.map((product, index) => {
-              return (
-                <Grid item lg={4} xs={12} sm={6} key={index}>
-                  <div className={styled.card}>
-                    <div className={styled.image_container}>
-                      <img
-                        src={product.image_thumb}
-                        className={styled.cardimage}
-                        alt={product.title}
-                      />
-                    </div>
-
-                    <div className={`styled.cardBody text-center`}>
-                      <h3 className={styled.cardTitle}>{product.name}</h3>
-                      <span>Rs: {product.price} </span>
-                    </div>
-                    <div className={styled.cardLink}>
-                      <Link to={`/products/${product.slug}`}>View Details</Link>
-                      {"  "}
-                      <Link to={`/products/${product.slug}`}>Add to Cart</Link>
-                    </div>
-                  </div>
-                </Grid>
-              )
-            })}
-          </Grid>
-        </div>
+        <ProductsList />
       </div>
     </Layout>
   )
